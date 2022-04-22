2681ab70891f670df384fdd4f71cf1d2ebeeaab8
Fozan Alkhalawi <fozanalkhalawi@gmail.com>
2022-04-23 01:56:59 +0300

    chore: add sample campus image

--- lib/core/constants/assets.dart
+++ lib/core/constants/assets.dart
@@ -2,4 +2,5 @@ class Assets {
   static const kLoadingAnimationAsset = 'assets/animations/loading_animation.json';
   static const kFreejLogoAsset = 'assets/images/freej_logo.png';
   static const kWavesAsset = 'assets/images/waves.png';
+  static const kKfupmCampusAsset = 'assets/images/kfupm_campus.jpeg';
 }

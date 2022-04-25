a611d37824110378dbb3fe0ac55b3899d414dd47
Fozan Alkhalawi <fozanalkhalawi@gmail.com>
2022-04-25 04:35:35 +0300

    feat: enhance announcement card

--- lib/app/announcement/componenets/announcement_card.dart
+++ lib/app/announcement/componenets/announcement_card.dart
@@ -10,53 +10,63 @@ class AnnouncementCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: Sizes.xxlCardHeight,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      // height: Sizes.xxlCardHeight,
+      child: Column(
         children: [
-          Text(
-            announcement.createdAt.dMMM,
-            style: TextStyles.body2.withColor(kPrimaryColor),
-          ),
-          const SizedBox(width: 10),
-          Column(
+          Row(
             children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.14 - (7.5 - 2) / 2,
+              ),
               Container(
                 height: 7.5,
                 width: 7.5,
                 decoration: const BoxDecoration(color: kPrimaryColor, shape: BoxShape.circle),
               ),
-              Container(
-                height: Sizes.xxlCardHeight - 7.5,
-                width: 1.5,
-                color: kPrimaryColor,
-              ),
             ],
           ),
-          const SizedBox(width: 20),
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  announcement.title,
-                  style: TextStyles.h2.withColor(kPrimaryColor),
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.14,
+                child: Text(
+                  announcement.createdAt.dMMM,
+                  style: TextStyles.body2.withColor(kPrimaryColor),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 10).relative(),
-                  child: Text(
-                    announcement.body,
-                    style: TextStyles.body2.withColor(kHintFontsColor),
-                    maxLines: 6,
-                    overflow: TextOverflow.ellipsis,
+              ),
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: const Border(left: BorderSide(color: kPrimaryColor, width: 2)).relative(),
+                  ),
+                  padding:
+                      const EdgeInsets.only(top: Insets.s, bottom: Insets.xxl * 1.5, left: Insets.m, right: Insets.m),
+                  child: Column(
+                    // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        announcement.title,
+                        style: TextStyles.h2.withColor(kPrimaryColor),
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      const SizedBox(height: 10),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10).relative(),
+                        child: Text(
+                          announcement.body,
+                          style: TextStyles.body2.withColor(kHintFontsColor),
+                          maxLines: 6,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                const SizedBox(),
-              ],
-            ),
+              ),
+            ],
           ),
         ],
       ),

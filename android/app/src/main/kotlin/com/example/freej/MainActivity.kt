c220551dada1ebab8c27f07ed9fe4fd1b027749b
Fozan Alkhalawi <fozanalkhalawi@gmail.com>
2022-04-27 01:54:31 +0300

    chore: update app bundle id

--- android/app/src/main/kotlin/com/example/freej/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/freej/MainActivity.kt
@@ -1,6 +1,5 @@
-package com.example.freej
+package com.freej
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity() {}
